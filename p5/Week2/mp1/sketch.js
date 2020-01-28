fb8ce1bf8acc3e8861aac2d69aba730675d64c85
emmill2 <emmill2@ilstu.edu>
2020-01-28 10:12:03 -0600

    Update sketch.js

--- p5/Week2/mp1/sketch.js
+++ p5/Week2/mp1/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   // put setup code here
-  createCanvas(800,800) ;
+  createCanvas(1550, 750) ;
 }
 
 function draw() {
@@ -8,14 +8,17 @@ function draw() {
   background('yellow') ;
 
 
+  textSize(40);
+  text(mouseX + ", " + mouseY, 100, 100)
+
 if (mouseIsPressed) {
   ellipse(50, 50, 50, 50);
 
 } else {
     rect(25, 25, 50, 50);
-  }
-}
+    ellipse(360, 315, 75, 75)
+    quad(333, 340,372, 344, 355, 495, 275, 465)
+    ellipse(310, 490, 100, 75)
 
-function mouseIsPressed() {
-  console.log(mouseX + ", " + mouseY) ;
+  }
 }
