a6c5b10dbac2919c02dec9164f20ace48bd4c966
emmill2 <emmill2@ilstu.edu>
2020-01-30 09:20:33 -0600

    Update sketch.js

--- p5/Week2/mp1/sketch.js
+++ p5/Week2/mp1/sketch.js
@@ -7,7 +7,7 @@ function draw() {
   // put drawing code here
   background('lightBlue') ;
 
-
+  fill('black');
   textSize(40);
   text(mouseX + ", " + mouseY, 100, 100)
 
@@ -18,36 +18,36 @@ if (mouseIsPressed) {
 
 
 // dino
-  fill('blue');
+  fill('lightGreen');
   ellipse(360, 215, 75, 75)
 
   //leg 1
-  fill('blue');
+  fill('lightGreen');
   quad(240, 365, 290, 395, 273, 524, 230, 535);
-  fill('blue');
+  fill('lightGreen');
   quad( 230, 535, 273, 524, 313, 544, 297, 553);
-  fill('blue');
+  fill('lightGreen');
   quad(313, 544, 312, 560, 240, 575, 250, 565);
-  fill('blue');
+  fill('lightGreen');
   quad(250, 565, 240, 575, 305, 583, 305, 569);
   //body and tail
-  fill('blue');
+  fill('lightGreen');
   quad(333, 240,372, 244, 305, 395, 205, 365);
-  fill('blue');
+  fill('lightGreen');
   triangle(205, 365, 260, 312, 95, 300);
 
   //leg 2
-  fill('blue');
+  fill('lightGreen');
   quad( 255, 365, 325, 350, 288, 524, 245, 535);
-  fill('blue');
+  fill('lightGreen');
   quad(245, 535, 288, 524, 328, 544,312, 553);
-  fill('blue');
+  fill('lightGreen');
   quad(328, 544, 327, 560, 255, 575, 265, 565);
-  fill('blue');
+  fill('lightGreen');
   quad(265, 565, 255, 575, 320, 583, 320, 569);
 
   //arms
-  fill('blue');
+  fill('lightGreen');
   quad(335, 295, 325, 305, 400, 313, 400, 299);
 
 }
@@ -56,21 +56,33 @@ else {
     fill('green');
     rect(0, 510, 775, 500);
 //dino
-    ellipse(360, 315, 75, 75)
+fill('lightGreen');
+    ellipse(360, 315, 75, 75);
     //leg 1
-    quad( 240, 465, 290, 495, 273, 524, 230, 535)
-    quad( 230, 535, 273, 524, 313, 544, 297, 553)
-    quad(313, 544, 312, 560, 240, 575, 250, 565)
-    quad(250, 565, 240, 575, 305, 583, 305, 569)
+    fill('lightGreen');
+    quad( 240, 465, 290, 495, 273, 524, 230, 535);
+    fill('lightGreen');
+    quad( 230, 535, 273, 524, 313, 544, 297, 553);
+    fill('lightGreen');
+    quad(313, 544, 312, 560, 240, 575, 250, 565);
+    fill('lightGreen');
+    quad(250, 565, 240, 575, 305, 583, 305, 569);
     //body and tail
-    quad(333, 340,372, 344, 305, 495, 205, 465)
-    triangle(205, 465, 260, 412, 95, 400)
+    fill('lightGreen');
+    quad(333, 340,372, 344, 305, 495, 205, 465);
+    fill('lightGreen');
+    triangle(205, 465, 260, 412, 95, 400);
     //leg 2
-    quad( 255, 465, 325, 450, 288, 524, 245, 535)
-    quad( 245, 535, 288, 524, 328, 544,312, 553)
-    quad(328, 544, 327, 560, 255, 575, 265, 565)
-    quad(265, 565, 255, 575, 320, 583, 320, 569)
+    fill('lightGreen');
+    quad( 255, 465, 325, 450, 288, 524, 245, 535);
+    fill('lightGreen');
+    quad( 245, 535, 288, 524, 328, 544,312, 553);
+    fill('lightGreen');
+    quad(328, 544, 327, 560, 255, 575, 265, 565);
+    fill('lightGreen');
+    quad(265, 565, 255, 575, 320, 583, 320, 569);
     //arms
-    quad(335, 395, 325, 405, 400, 413, 400, 399)
+    fill('lightGreen');
+    quad(335, 395, 325, 405, 400, 413, 400, 399);
   }
 }
