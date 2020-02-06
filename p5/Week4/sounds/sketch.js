2672a4b367429ff63ffab36e8941057f55028a9e
emmill2 <emmill2@ilstu.edu>
2020-02-06 09:19:43 -0600

    Update sketch.js

--- p5/Week4/sounds/sketch.js
+++ p5/Week4/sounds/sketch.js
@@ -1,13 +1,17 @@
 var song1;
+
 function preload() {
-  song1 = loadSound("assets/cowboys.wav")
+  song1 = loadSound('assets/cowboys.wav');
 }
+
 function setup() {
-  // put setup code here
-  createCanvas(800,800);
-  song1.play
+
+  createCanvas(720, 200);
+  background(255, 0, 0);
+  song1.play();  // this is what starts the sound
+
 }
 
 function draw() {
-  // put drawing code here
+
 }
